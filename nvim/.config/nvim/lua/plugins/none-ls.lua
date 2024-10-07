4b30a0dea2c6fed68b35416915076ac9b5631e78
Yetso <dav.catoul@gmail.com>
2024-10-07 15:39:35 +0200

    added nix formatting and tabs

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -12,6 +12,7 @@ return {
 					null_ls.builtins.formatting.google_java_format.with({
 						extra_args = {"--set-tabs", "false", "--tab-size", "4"},
 					}),
+					null_ls.builtins.formatting.nixpkgs_fmt,
 					-- null_ls.builtins.formatting.black,
 					-- null_ls.builtins.formatting.isort,
 				},
