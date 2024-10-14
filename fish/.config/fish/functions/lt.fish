e0ebb9ed87a73b2cebff6e1f9945c37355e154dd
Yetso <dav.catoul@gmail.com>
2024-10-14 15:10:41 +0200

    change eza tree to 3 level instead of 2

--- fish/.config/fish/functions/lt.fish
+++ fish/.config/fish/functions/lt.fish
@@ -1,4 +1,4 @@
-function lt --wraps=eza\ --long\ --tree\ --level=2\ --all\ --header\ --binary\ --color=always\ --group-directories-first\ --icons=always\ --ignore-glob=\'.DS_Store\'\ --no-quotes --description alias\ lt=eza\ --long\ --tree\ --level=2\ --all\ --header\ --binary\ --color=always\ --group-directories-first\ --icons=always\ --ignore-glob=\'.DS_Store\'\ --no-quotes
-  eza --long --tree --level=2 --all --header --binary --color=always --group-directories-first --icons=always --ignore-glob='.DS_Store' --no-quotes $argv
+function lt --wraps=eza\ --long\ --tree\ --level=3\ --all\ --header\ --binary\ --color=always\ --group-directories-first\ --icons=always\ --ignore-glob=\'.DS_Store\'\ --no-quotes --description alias\ lt=eza\ --long\ --tree\ --level=2\ --all\ --header\ --binary\ --color=always\ --group-directories-first\ --icons=always\ --ignore-glob=\'.DS_Store\'\ --no-quotes
+  eza --long --tree --level=3 --all --header --binary --color=always --group-directories-first --icons=always --ignore-glob='.DS_Store|.git' --no-quotes $argv
         
 end
