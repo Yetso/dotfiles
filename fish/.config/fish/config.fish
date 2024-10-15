d5e3c979892c917ffbd3a37caedc8a57ca53ff15
Yetso <dav.catoul@gmail.com>
2024-10-15 17:22:55 +0200

    change nvim alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	abbr --add lg lazygit
+	abbr --add v nvim
+
+	starship init fish | source
+	zoxide init fish --cmd cd | source
 end
 
-abbr --add lg lazygit
 
-starship init fish | source
-zoxide init fish --cmd cd | source
-
-
-
-# fastfetch
--- fish/.config/fish/functions/v.fish
+++
@@ -1,4 +0,0 @@
-function v --wraps=nvim --description 'alias v=nvim'
-  nvim $argv
-        
-end
