36cd496e2884523539bb502d90f417f4f93ec747
Yetso <dav.catoul@gmail.com>
2024-10-20 20:41:42 +0200

    modify update function to include nix-darwin

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,4 +1,9 @@
-function update --wraps='open /Applications/Latest.app;brew update;brew upgrade;brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest' --description 'alias update=open /Applications/Latest.app;brew update;brew upgrade;brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest'
-  open /Applications/Latest.app;brew update;brew upgrade;brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest $argv
-        
+function update --wraps='open /Applications/Latest.app;brew update;brew upgrade;brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest' --description 'alias update=open /Applications/Latest.app;brew update;brew upgrade;brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest; nix flake update'
+  open /Applications/Latest.app
+  brew update
+  brew upgrade
+  brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest
+  cd ~/.config/nix-darwin/
+  nix flake update --commit-lock-file
+  darwin-rebuild switch
 end
