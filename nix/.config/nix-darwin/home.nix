328127c5bf6067061864b77616b01e55da66fdc7
Yetso <dav.catoul@gmail.com>
2024-10-20 22:01:23 +0200

    try path

--- nix/.config/nix-darwin/home.nix
+++ nix/.config/nix-darwin/home.nix
@@ -6,12 +6,12 @@
   home.stateVersion = "24.05";
 
   home.file = {
-    ".config/starship".source = dotfiles/starship;
-    ".config/bat".source = dotfiles/bat;
-    ".config/fastfetch".source = dotfiles/fastfetch;
-    ".config/wezterm".source = dotfiles/wezterm/.config;
-    ".wezterm-completion.sh".source = dotfiles/wezterm;
-    ".terminfo".source = dotfiles/wezterm/.terminfo;
+    ".config/starship".source = "~/dotfiles/starship";
+    ".config/bat".source = "~/dotfiles/bat";
+    ".config/fastfetch".source = "~/dotfiles/fastfetch";
+    ".config/wezterm".source = "~/dotfiles/wezterm/.config";
+    ".wezterm-completion.sh".source = "~/dotfiles/wezterm";
+    ".terminfo".source = "~/dotfiles/wezterm/.terminfo";
   };
 
   home.packages = [
