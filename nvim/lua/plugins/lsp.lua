1b45b9abe8a27156b209c950b3d4604644ac613b
Yetso <dav.catoul@gmail.com>
2024-12-07 16:00:44 +0100

    change comments

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -34,9 +34,8 @@ return {
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 			end
 			require("mason-lspconfig").setup_handlers({
-				-- The first entry (without a key) will be the default handler
-				-- and will be called for each installed server that doesn't have
-				-- a dedicated handler.
+				-- The first entry (without a key) will be the default handler and will
+				-- be called for each installed server that doesn't have a dedicated handler.
 				function(server_name) -- default handler (optional)
 					require("lspconfig")[server_name].setup({
 						on_attach = lsp_attach,
@@ -52,10 +51,6 @@ return {
 						},
 					})
 				end,
-				-- Next, you can provide a dedicated handler for specific servers.
-				-- ["lua_ls"] = function()
-				-- 	require("luasnip").setup {}
-				-- end
 			})
 		end,
 	},
