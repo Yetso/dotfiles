6943ea5528f96b800e45747f2683ca210b9bd540
Yetso <dav.catoul@gmail.com>
2025-01-24 12:27:57 +0100

    remove google java format

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,9 +9,6 @@ return {
 			return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.google_java_format.with({
-						extra_args = {"--set-tabs", "false", "--tab-size", "4"},
-					}),
 					null_ls.builtins.formatting.nixpkgs_fmt,
 					-- null_ls.builtins.formatting.black,
 					-- null_ls.builtins.formatting.isort,
