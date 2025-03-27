47b3dbb6cdc03ac93ea9184f5ff60537af7f4cd1
Yetso <dav.catoul@gmail.com>
2025-03-27 22:41:22 +0100

    add snacks.nvim and replace some plugins by it + UFO.nvim for collapse

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,17 +3,20 @@ return {
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		{
-			"rcarriga/nvim-notify",
-			opts = {
-				background_colour = "#000000",
-			},
+			-- "rcarriga/nvim-notify",
+			-- opts = {
+			-- 	background_colour = "#000000",
+			-- },
 		},
 	},
 	event = "VeryLazy",
 	opts = {
+		notify = {
+			enabled = false,
+		},
 		lsp = {
 			progress = {
-				enable = true,
+				enabled = false,
 			},
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -27,23 +30,23 @@ return {
 			long_message_to_split = true,
 			lsp_doc_border = true,
 		},
-		routes = {
-			{
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B written" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-						{ find = "%d fewer lines" },
-						{ find = "%d more lines" },
-						{ find = "line less;" },
-						{ find = "lines yanked" },
-						{ find = "/" },
-					},
-				},
-				opts = { skip = true },
-			},
-		},
+		-- routes = {
+		-- 	{
+		-- 		filter = {
+		-- 			event = "msg_show",
+		-- 			any = {
+		-- 				{ find = "%d+L, %d+B written" },
+		-- 				{ find = "; after #%d+" },
+		-- 				{ find = "; before #%d+" },
+		-- 				{ find = "%d fewer lines" },
+		-- 				{ find = "%d more lines" },
+		-- 				{ find = "line less;" },
+		-- 				{ find = "lines yanked" },
+		-- 				{ find = "/" },
+		-- 			},
+		-- 		},
+		-- 		opts = { skip = true },
+		-- 	},
+		-- },
 	},
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,18 +1,19 @@
 return {
 	{ -- Fuzzy Finder (files, lsp, etc)
 		'nvim-telescope/telescope.nvim',
+		enabled = false,
 		event = 'VimEnter',
 		version = false,
 		cmd = "Telescope",
 		dependencies = {
 			'nvim-lua/plenary.nvim',
-			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'make',
-				cond = function()
-					return vim.fn.executable 'make' == 1
-				end,
-			},
+			-- { -- If encountering errors, see telescope-fzf-native README for installation instructions
+			-- 	'nvim-telescope/telescope-fzf-native.nvim',
+			-- 	build = 'make',
+			-- 	cond = function()
+			-- 		return vim.fn.executable 'make' == 1
+			-- 	end,
+			-- },
 			{ 'nvim-telescope/telescope-ui-select.nvim' },
 
 			{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
@@ -20,6 +21,7 @@ return {
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc="[F]ind [F]iles"},
 			{ "<C-p>", "<cmd>Telescope git_files<cr>", desc="[p]roject files (.git)"},
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc="[F]ind in code"},
 		},
 		config = function()
 			-- [[ Configure Telescope ]]
@@ -31,7 +33,8 @@ return {
 					},
 				},
 				defaults = {
-					path_display = {"truncate"},
+					layout_strategy = 'vertical',
+					path_display = {truncate = 10},
 				},
 				pickers = {
 					lsp_references = {
@@ -46,9 +49,9 @@ return {
 			}
 
 			-- Enable Telescope extensions if they are installed
-			require('telescope').load_extension('fzf')
+			-- require('telescope').load_extension('fzf')
 			require('telescope').load_extension('ui-select')
-			require('telescope').load_extension('noice')
+			-- require('telescope').load_extension('noice')
 		end,
 	},
 }
