f9c6563edd6525a38f8ccf17537bea4addc3109b
Yetso <dav.catoul@gmail.com>
2025-03-31 12:38:02 +0200

    removed ftplugin

--- nvim/ftplugin/java.lua
+++
@@ -1,7 +0,0 @@
--- local jdtls_path = require("mason-registry").get_package("jdtls"):get_install_path()
---
--- local config = {
---     cmd = {jdtls_path},
---     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
--- }
--- require('jdtls').start_or_attach(config)
