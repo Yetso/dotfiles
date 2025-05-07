400e1502d6a5133e3c9b3bfe9c6b5b765263946f
Yetso <dav.catoul@gmail.com>
2025-05-07 15:58:36 +0200

    remove setup_handler from mason-lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,19 @@
+-- Raccourci pour LSP
+local bufopts = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, bufopts)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+vim.keymap.set("n", "<leader>gr",
+	function() Snacks.picker.lsp_references({ auto_confirm = false, layout = { preset = "vertical", layout = { width = 0.8 } } }) end,
+	bufopts)
+vim.keymap.set("n", "<leader>gd", function() Snacks.picker.lsp_definitions() end, bufopts)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+vim.keymap.set("n", "E", function()
+	vim.diagnostic.open_float(nil, {
+		focusable = true,
+		border = "rounded"
+	})
+end, { desc = "Afficher les erreurs LSP avec une bordure" })
 ---@diagnostic disable: undefined-global
 return {
 	{
@@ -5,7 +21,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			-- { "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+			{ "mason-org/mason-lspconfig.nvim" },
 			{
 				"folke/lazydev.nvim",
 				ft = "lua", -- only load on lua files
@@ -20,46 +36,8 @@ return {
 		},
 		config = function()
 			-- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lsp_attach = function(_, bufnr)
 				-- Create your keybindings here
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
 				-- local telescopeUi = require("telescope.builtin")
-
-				-- Raccourci pour LSP
-				vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, bufopts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-				vim.keymap.set("n", "<leader>gr",
-					function() Snacks.picker.lsp_references({ auto_confirm = false, layout = { preset = "vertical", layout = { width = 0.8 } } }) end,
-					bufopts)
-				vim.keymap.set("n", "<leader>gd", function() Snacks.picker.lsp_definitions() end, bufopts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-				vim.keymap.set("n", "E", function()
-					vim.diagnostic.open_float(nil, {
-						focusable = true,
-						border = "rounded"
-					})
-				end, { desc = "Afficher les erreurs LSP avec une bordure" })
-			end
-			require("mason-lspconfig").setup_handlers({
-				-- The first entry (without a key) will be the default handler and will
-				-- be called for each installed server that doesn't have a dedicated handler.
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({
-						on_attach = lsp_attach,
-					})
-				end,
-				["omnisharp"] = function()
-					local mason_registry = require("mason-registry")
-					local omnisharp_path = mason_registry.get_package("omnisharp"):get_install_path() .. "/omnisharp"
-
-					require("lspconfig").omnisharp.setup({
-						cmd = { omnisharp_path }, -- Chemin vers l'exécutable d'OmniSharp
-						enable_roslyn_analyzers = true,
-						on_attach = lsp_attach,
-					})
-				end,
-			})
 		end,
 	},
 	{
