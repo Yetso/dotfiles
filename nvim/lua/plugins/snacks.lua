cad38bb6cc23a00a437bd402d14adbd5428944b8
Yetso <dav.catoul@gmail.com>
2025-05-16 15:35:00 +0200

    add function for debug with snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -55,6 +55,21 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Setup some globals for debugging (lazy-loaded)
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				vim.print = _G.dd -- Override print to use snacks for `:=` command
+			end,
+		})
+	end,
 	opts = {
 		statuscolumn = {
 			enabled = true,
