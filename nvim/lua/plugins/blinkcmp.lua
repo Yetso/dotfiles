a4bf65b1e89d66ed768ff461082df1c70e2a8df9
Yetso <dav.catoul@gmail.com>
2025-06-01 09:40:09 +0200

    modify blink.nvim options

--- nvim/lua/plugins/blinkcmp.lua
+++ nvim/lua/plugins/blinkcmp.lua
@@ -19,6 +19,28 @@ return {
 			list = {
 				selection = { preselect = false, auto_insert = false }
 			},
+			menu = {
+				border = nil,
+				scrolloff = 1,
+				scrollbar = false,
+				draw = {
+					columns = {
+						{ "kind_icon" },
+						{ "label",      "label_description", gap = 1 },
+						{ "kind" },
+						{ "source_name" },
+					},
+				},
+			},
+			documentation = {
+				window = {
+					border = nil,
+					scrollbar = false,
+					winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
+				},
+				auto_show = true,
+				auto_show_delay_ms = 500,
+			},
 		},
 		cmdline = {
 			completion = {
