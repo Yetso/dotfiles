2b3d41fb1f8a58ded9b7d309d8995bfd5d44cedd
Yetso <dav.catoul@gmail.com>
2025-06-01 09:40:52 +0200

    removed gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- "lewis6991/gitsigns.nvim",
-	-- event = {"BufReadPre", "BufNewFile"},
-	-- config = true,
-	-- keys = {
-	-- 	{ "<leader>g", "<CMD>Gitsigns preview_hunk<CR>", desc = "Git view changes" },
-	-- },
-}
