12a3119309da57b0c11650cbfa18496749c59c6f
Yetso <dav.catoul@gmail.com>
2025-06-01 09:39:02 +0200

    add autocmd to cd when entering nvim

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -47,6 +47,38 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	pattern = '*',
 })
 
+-- Change vers le répertoire du premier fichier ouvert au démarrage
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "*",
+	callback = function()
+		if vim.fn.argc() < 1 then
+			return
+		end
+		local first_arg = vim.fn.argv(0)
+
+		-- Gérer le cas où argv retourne un array
+		if type(first_arg) == "table" then
+			first_arg = first_arg[1]
+		end
+
+		if type(first_arg) ~= "string" or first_arg == "" then
+			return
+		end
+		-- Convertir en chemin absolu
+		local abs_path = vim.fn.fnamemodify(first_arg, ":p")
+
+		local target_dir
+		if vim.fn.isdirectory(abs_path) == 1 then
+			-- Si c'est un dossier, utiliser directement
+			target_dir = abs_path
+		else
+			-- Si c'est un fichier, prendre son répertoire parent
+			target_dir = vim.fn.fnamemodify(abs_path, ":h")
+		end
+		vim.cmd("cd " .. vim.fn.fnameescape(target_dir))
+	end
+})
+
 vim.opt.laststatus = 3
 vim.opt.showcmd = false
 
@@ -54,12 +86,15 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
+vim.opt.textwidth = 100
+vim.opt.formatoptions:remove("t")
+
 -- Tabs for nix files
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "nix",
 	callback = function()
 		vim.opt_local.expandtab = true -- Use spaces instead of tabs
-		vim.opt_local.tabstop = 2  -- Number of spaces a <Tab> counts for
+		vim.opt_local.tabstop = 2 -- Number of spaces a <Tab> counts for
 		vim.opt_local.shiftwidth = 2 -- Number of spaces to use for (auto)indent
 		vim.opt_local.softtabstop = 2 -- Number of spaces a <Tab> counts for while editing
 	end,
@@ -96,7 +131,7 @@ vim.diagnostic.config({
 	}
 })
 
-vim.opt.guicursor = "n-v-c:block-Cursor,i:ver25-Cursor,r:hor20-Cursor"
+vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve-t:ver25-Cursor,r:hor20-Cursor"
 
 -- print checkhealth with float window
 vim.g.health = { style = 'float' }
