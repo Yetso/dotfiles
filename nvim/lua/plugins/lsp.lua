182dc6eb01affba3f527346a3218d7f4cb40bea1
Yetso <dav.catoul@gmail.com>
2025-08-12 11:08:16 +0200

    correct Mason

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,30 +1,28 @@
-vim.lsp.enable({
-	"jdtls",
-	"lua_ls",
-	"tinymist",
-	"jsonls",
-})
-
-
-vim.keymap.set("n", "grr", function() Snacks.picker.lsp_references({ focus = "list" }) end,
-	{ noremap = true, silent = true, desc = "Go to LSP references" })
-
-vim.keymap.set("n", "gre", function() vim.diagnostic.open_float(nil, { focusable = true }) end,
-	{ noremap = true, silent = true, desc = "Print LSP messages" })
 
 
 return {
-	{
-		"mason-org/mason.nvim",
-		build = ":MasonUpdate",
-		opts = {
-			ensure_installed = {
-				"stylua",
-				-- "basedpyright",
-				-- "google-java-format",
-				-- "jdtls",
-				"lua-language-server",
+	"mason-org/mason-lspconfig.nvim",
+	opts = {},
+	dependencies = {
+		{
+			"mason-org/mason.nvim",
+			-- cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+			build = ":MasonUpdate",
+			opts = {},
+		},
+		{
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			opts = {
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"tinymist",
+					"google-java-format",
+					"jdtls",
+					"json-lsp",
+					"typstyle",
+				}
 			},
 		},
-	},
+	}
 }
