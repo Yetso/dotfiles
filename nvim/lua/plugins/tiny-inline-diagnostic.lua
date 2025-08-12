7ef882a34d630b4cf2b4c39e4f56aece4fdfad4a
Yetso <dav.catoul@gmail.com>
2025-08-12 11:07:18 +0200

    change cursorLine bug

--- nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -3,13 +3,11 @@ return {
 	event = "VeryLazy", -- Or `LspAttach`
 	priority = 1000, -- needs to be loaded in first
 	opts = {
-		transparent_cursorline = true,
 		options = {
 			use_icons_from_diagnostic = true,
 		},
+		hi = {
+			background = "#1C1C1C",
+		},
 	},
-	config = function(_, opts)
-		require('tiny-inline-diagnostic').setup(opts)
-		vim.api.nvim_set_hl(0, "TinyInlineDiagnosticVirtualTextArrow", {link = "CursorLine"})
-	end,
 }
