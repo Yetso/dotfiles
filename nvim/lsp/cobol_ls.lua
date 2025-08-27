04f6df53e09512b74f1031381a7106d6d7ce6248
Yetso <dav.catoul@gmail.com>
2025-08-27 09:00:27 +0200

    add LSPconfig

--- nvim/lsp/cobol_ls.lua
+++
@@ -1,8 +0,0 @@
----@brief
----
---- Cobol language support
-return {
-  cmd = { 'cobol-language-support' },
-  filetypes = { 'cobol' },
-  root_markers = { '.git' },
-}
--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -67,24 +67,19 @@
 --- * [Lua.runtime.path](https://luals.github.io/wiki/settings/#runtimepath)
 --- * [Lua.workspace.library](https://luals.github.io/wiki/settings/#workspacelibrary)
 ---
-return {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = {
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
-  settings = {
-    Lua = {
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-}
+
+vim.lsp.config('lua_ls', {
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+			},
+		},
+	},
+})
