0d3d9185ee4eeb70cd5efe568ceee6da23fec1db
Yetso <dav.catoul@gmail.com>
2026-01-13 10:18:56 +0100

    change LSP config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,15 @@
-vim.filetype.add({
-	pattern = {
-		["hypr.*%.conf"] = "hyprlang",
-	},
-})
+local lsps = {
+	"google-java-format",
+	"gopls",
+	"hyprls",
+	"jdtls",
+	"json-lsp",
+	"lua-language-server",
+	"python-lsp-server",
+	"markdown-oxide",
+	"stylua",
+	"tinymist",
+}
 
 return {
 	{
@@ -11,29 +18,46 @@ return {
 		dependencies = {
 			{
 				"mason-org/mason.nvim",
-				-- cmd = { "Mason", "MasonInstall", "MasonUpdate" },
 				build = ":MasonUpdate",
 				opts = {},
+				config = function(_, opts)
+					require("mason").setup(opts)
+
+					local registry = require("mason-registry")
+
+					for _, pkg in ipairs(lsps) do
+						if registry.has_package(pkg) then
+							if not registry.is_installed(pkg) then
+								vim.cmd("MasonInstall " .. pkg)
+							end
+						else
+							vim.notify("Mason package not found: " .. pkg, vim.log.levels.WARN)
+						end
+					end
+				end,
 			},
 			{
-				"WhoIsSethDaniel/mason-tool-installer.nvim",
-				opts = {
-					ensure_installed = {
-						"google-java-format",
-						"gopls",
-						"jdtls",
-						"json-lsp",
-						"lua_ls",
-						"pylsp",
-						"stylua",
-						"tinymist",
-						-- "typstyle",
-					},
-				},
+				"neovim/nvim-lspconfig",
 			},
 		},
 	},
 	{
-		"neovim/nvim-lspconfig",
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "LspAttach", -- Or `LspAttach`
+		priority = 1000, -- needs to be loaded in first
+		opts = {
+			options = {
+				use_icons_from_diagnostic = true,
+			},
+			hi = {
+				background = "#1C1C1C",
+			},
+		},
+		config = function(_, opts)
+			require("tiny-inline-diagnostic").setup(opts)
+			vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
+			vim.api.nvim_set_hl(0, "TinyInlineDiagnosticVirtualTextArrow", { bg = "#1C1C1C" })
+		end,
 	},
 }
+
