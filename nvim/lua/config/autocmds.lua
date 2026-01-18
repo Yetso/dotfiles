d6d53d0aeb4021324db3dafedf61a9242a7c1a58
Yetso <dav.catoul@gmail.com>
2026-01-18 18:02:07 +0100

    add quit autocmd with snacks explorer

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,5 +1,7 @@
 -- add Update command to shorten vim.pack.update()
-vim.api.nvim_create_user_command("Update", function() vim.pack.update() end, {})
+vim.api.nvim_create_user_command("Update", function()
+	vim.pack.update()
+end, {})
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -46,10 +48,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	end,
 })
 
--- local function is_floating_window()
--- 	local config = vim.api.nvim_win_get_config(0)
--- 	return config.relative ~= ""
--- end
 -- Autocommand pour appliquer le remap uniquement si ce n'est pas LazyGit (ou autre fenêtre flottante)
 vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "*",
@@ -65,6 +63,28 @@ vim.api.nvim_create_autocmd("TermOpen", {
 				[[<C-\><C-n>]],
 				{ noremap = true, silent = true, desc = "quit insert mode inside terminal" }
 			)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("QuitPre", {
+	callback = function()
+		local snacks_windows = {}
+		local floating_windows = {}
+		local windows = vim.api.nvim_list_wins()
+		for _, w in ipairs(windows) do
+			local filetype = vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_win_get_buf(w) })
+			if filetype:match("snacks_") ~= nil then
+				table.insert(snacks_windows, w)
+			elseif vim.api.nvim_win_get_config(w).relative ~= "" then
+				table.insert(floating_windows, w)
+			end
+		end
+		if 1 == #windows - #floating_windows - #snacks_windows then
+			-- Should quit, so we close all Snacks windows.
+			for _, w in ipairs(snacks_windows) do
+				vim.api.nvim_win_close(w, true)
+			end
 		end
 	end,
 })
