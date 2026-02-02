974e275c062cc0e5fac038f004d4aeafd0cb2c80
Yetso <dav.catoul@gmail.com>
2026-02-02 17:58:35 +0100

    fix jdtls.lua formatting

--- nvim/lsp/jdtls.lua
+++ nvim/lsp/jdtls.lua
@@ -34,30 +34,30 @@
 
 local env = {
 	HOME = vim.uv.os_homedir(),
-	XDG_CACHE_HOME = os.getenv 'XDG_CACHE_HOME',
-	JDTLS_JVM_ARGS = os.getenv 'JDTLS_JVM_ARGS',
+	XDG_CACHE_HOME = os.getenv("XDG_CACHE_HOME"),
+	JDTLS_JVM_ARGS = os.getenv("JDTLS_JVM_ARGS"),
 }
 
 local function get_cache_dir()
-	return env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or env.HOME .. '/.cache'
+	return env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or env.HOME .. "/.cache"
 end
 
 local function get_jdtls_cache_dir()
-	return get_cache_dir() .. '/jdtls'
+	return get_cache_dir() .. "/jdtls"
 end
 
 local function get_jdtls_config_dir()
-	return get_jdtls_cache_dir() .. '/config'
+	return get_jdtls_cache_dir() .. "/config"
 end
 
 local function get_jdtls_workspace_dir()
-	return get_jdtls_cache_dir() .. '/workspace'
+	return get_jdtls_cache_dir() .. "/workspace"
 end
 
 local function get_jdtls_jvm_args()
 	local args = {}
-	for a in string.gmatch((env.JDTLS_JVM_ARGS or ''), '%S+') do
-		local arg = string.format('--jvm-arg=%s', a)
+	for a in string.gmatch((env.JDTLS_JVM_ARGS or ""), "%S+") do
+		local arg = string.format("--jvm-arg=%s", a)
 		table.insert(args, arg)
 	end
 	return unpack(args)
@@ -65,10 +65,10 @@ end
 
 return {
 	cmd = {
-		'jdtls',
-		'-configuration',
+		"jdtls",
+		"-configuration",
 		get_jdtls_config_dir(),
-		'-data',
+		"-data",
 		get_jdtls_workspace_dir(),
 		"--jvm-arg=-javaagent:" .. env.HOME .. "/.local/share/nvim/mason/share/jdtls/lombok.jar",
 		get_jdtls_jvm_args(),
@@ -89,7 +89,7 @@ return {
 				"extractMethod",
 				"extractVariable",
 				"extractConstant",
-				"extractVariableAllOccurrence"
+				"extractVariableAllOccurrence",
 			},
 		},
 	},
